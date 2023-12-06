7a7a6f088edd7e58dcf93dd0a1e7c0c231c6fc47
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-06 05:49:01 +0000

    Operations_discuss

---
+++ basics/conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "aniket";
+
+// console.log(typeof score); // string
+// console.log(typeof(score)); // string
+
+ let scores = 33;
+ let valueNumber = Number(scores)
+console.log(typeof valueNumber);// print number 
+console.log(valueNumber); //print Nan => not a number
+
+let scoress = "33abc";
+let valueInNumber = Number(scoress)
+console.log(typeof valueInNumber);// print number 
+console.log(valueInNumber); //print NaN 
+
+
+let scor = null;
+let valueOnNumber = Number(scor)
+console.log(typeof valueOnNumber);// print number
+console.log(valueOnNumber); // print 0
+
+let isLoggedIn = "Aniket"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);// print true
+
+// 1 = true; 0 = false;
+// "" = false
+// "Aniket" = true
+
+let someNumber = 33
+let someValue = String(someNumber)
+console.log(typeof someValue); // print string
+
+
