2817f850dc39fe498b39bc727ea6d67a8fecb84c
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-08 15:32:40 +0000

    operators

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -1,27 +1,27 @@
 let score = "aniket";
 
-// console.log(typeof score); // string
+console.log(typeof score); // string
 // console.log(typeof(score)); // string
 
  let scores = 33;
  let valueNumber = Number(scores)
-console.log(typeof valueNumber);// print number 
-console.log(valueNumber); //print Nan => not a number
+// console.log(typeof valueNumber);// print number 
+// console.log(valueNumber); //print Nan => not a number
 
 let scoress = "33abc";
 let valueInNumber = Number(scoress)
-console.log(typeof valueInNumber);// print number 
-console.log(valueInNumber); //print NaN 
+// console.log(typeof valueInNumber);// print number 
+// console.log(valueInNumber); //print NaN 
 
 
 let scor = null;
 let valueOnNumber = Number(scor)
-console.log(typeof valueOnNumber);// print number
-console.log(valueOnNumber); // print 0
+// console.log(typeof valueOnNumber);// print number
+// console.log(valueOnNumber); // print 0
 
 let isLoggedIn = "Aniket"
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);// print true
+// console.log(booleanisLoggedIn);// print true
 
 // 1 = true; 0 = false;
 // "" = false
@@ -29,6 +29,25 @@ console.log(booleanisLoggedIn);// print true
 
 let someNumber = 33
 let someValue = String(someNumber)
-console.log(typeof someValue); // print string
+// console.log(typeof someValue); // print string
+
+/*******************operator*************************/
+
+let value = 3 
+let negative = -value;
+// console.log(negative);
+
+let str1 = "hello";
+let str2 = " Aniket";
+let str3 = (str1+str2);
+// console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"3");//32
+
+// console.log(+true);//TRUE
+// console.log(+"");//0
 
 
