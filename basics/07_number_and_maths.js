cfd71f6900ddec509e7af796024c17f0e365c071
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-08 17:19:49 +0000

    math and num

---
+++ basics/07_number_and_maths.js
@@ -0,0 +1,56 @@
+//NUMBER....................
+const score = 400
+// console.log(score);// only 400
+
+const balance = new Number(100)
+// console.log(balance);//type number:100
+
+// console.log(balance.toString().length);// 3
+
+// console.log(balance.toFixed(2));// print value 100.00
+
+
+const otherNumber = 23.866
+// console.log(otherNumber.toPrecision(3));// total three value in focus 23.9
+
+
+
+const hunders = 100000
+// console.log(hunders.toLocaleString('en-IN'));// print value 10,00,000 INDIAN VALUE WITH COMMOS
+
+// Maths......................
+console.log(Math);
+
+console.log(Math.abs(-4));// print positive value 4
+
+console.log(Math.round(4.3));// 4,4
+
+console.log(Math.ceil(4.2));// 5
+console.log(Math.floor(4.9));// 4
+
+console.log(Math.min(4,3,2,6));// 2
+console.log(Math.max(4,3,2,6));// 6
+
+console.log(Math.random());// print 0 to 1 value 
+
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *  (max-min +1)) + min);// 10 to 20 
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
