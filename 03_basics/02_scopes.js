3ee9cfb99468bde1c86da4cccb3a78eaf08d9d03
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-09 17:04:43 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,7 +9,7 @@ if(true){
     let a = 10
     const b =20
     // var c = 30
-    console.log("inner:",a);
+    // console.log("inner:",a);
 }
 
 // for(let i =0; i<Array.length; i++){
@@ -18,6 +18,47 @@ if(true){
 
 
 
-console.log(a);
-console.log(b);
-console.log(c);//30
+// console.log(a);
+// console.log(b);
+// console.log(c);//30
+
+// nested scope................
+
+function one (){
+    const userName = "Aniket"
+    function two(){
+        const website = "youtube"
+        // console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "aniket"
+    if(username === "aniket"){
+      const website1 = " youtube"
+    //   console.log(username + website1);
+    }
+    // console.log(website1);
+}
+// console.log(username);
+
+
+
+// +++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+// console.log(addone(5));
+
+
+addTwo(6)
+const addTwo = function(num){
+    return num + 2
+}
