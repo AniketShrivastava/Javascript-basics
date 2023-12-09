695882d418447d175a5c1d29b6a713a6184ca79e
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-09 05:03:37 +0000

    objects

---
+++ 02_basics/04_object.js
@@ -0,0 +1,61 @@
+const tinderUser = new Object()
+console.log(tinderUser);// {}
+
+ tinderUser.id ="123abc"
+ tinderUser.name = "ani"
+ tinderUser.isLoggedIn = false
+ console.log(tinderUser);// all value print
+
+ const regularUser = {
+    email:"aniket@gmail.com",
+    fullname:{
+        userfullname:{
+            fristname:"aniket",
+            lastname:"shrivastava"
+        }
+    }
+ }
+
+ console.log(regularUser.fullname.userfullname.fristname);// aniket
+
+
+ const obj1 = {1:"a",2:"b"}
+ const obj2 = {3:"a",4:"b"}
+
+//  const obj3 = {obj1,obj2}
+//  const obj3 = object.assign(obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+
+ console.log(obj3);
+
+ const users = [
+    {
+        id:1,
+        email:"a@gmail.com"
+    },{
+        id:1,
+        email:"a@gmail.com"
+    },{
+        id:1,
+        email:"a@gmail.com"
+    }
+ ]
+
+ users[1].email
+
+ console.log(tinderUser);
+ console.log(object.key(tinderUser));
+ console.log(object.values(tinderUser));
+ console.log(object.entries(tinderUser));
+
+
+ console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
+ 
+ 
