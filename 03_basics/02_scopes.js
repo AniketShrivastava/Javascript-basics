3ed58e2d8e864597028f0102342f3eb98d581fee
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-09 16:45:23 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+// let a = 10
+// const b =20
+// var c = 30
+
+// var c = 300 
+let a = 300
+
+if(true){
+    let a = 10
+    const b =20
+    // var c = 30
+    console.log("inner:",a);
+}
+
+// for(let i =0; i<Array.length; i++){
+//     const element = Array[i];
+// }
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);//30
