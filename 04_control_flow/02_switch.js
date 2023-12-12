a7441189315deacafafa994fd732aefb29231559
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-12 05:59:55 +0000

    switch_Operator

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,52 @@
+//  switch.................
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("januvary");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }// 3 is march
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}//print march
