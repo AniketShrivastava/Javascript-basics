11843a8365dc0159b66ae815a6ed3d1ce63d20e6
Aniket shrivasatava <133022217+AniketShrivastava@users.noreply.github.com>
2023-12-13 04:26:05 +0000

    while and do-while loop

---
+++ 05_iteration/02_two.js
@@ -0,0 +1,29 @@
+//  while loop ......................
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value is index is ${index}`);//print 2 table
+    index = index + 2
+}
+
+// while loop.............................
+
+
+let myArray = ["flash","batman","superman"]
+let arr = 0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);//print flash , batman,superman
+    arr = arr + 1
+}
+
+// do-while loop ........................
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+
+} while (score<=10);// print all score 1 to 10.......
+
+// print score 11 like installment ............
